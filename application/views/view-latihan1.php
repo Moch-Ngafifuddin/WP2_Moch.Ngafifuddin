c9ef183cb1a54916395c5a3637a2f92289194a7f
Moch-Ngafifuddin <moch.ngafifuddin20@gmail.com>
2023-10-16 12:03:59 +0700

    Menambah kan 3 file latihan 1 pada folder controller,model_latihan 1 pada folder model,view_latihan1 pada folder fiew

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan1</title>
+</head>
+<body>
+    Hallo Kawan.. Yuk kita belajar web programing..!!! <br>
+    Nilai 1 =<?= $nilai1; ?>
+    Nilai 2 =<?= $nilai2; ?>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu
+    <?= $nilai1. "+" . $nilai2 . "=" . $hasil ?>
+</body>
+</html>
