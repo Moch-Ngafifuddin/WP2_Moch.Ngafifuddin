fe65003b8d4074a1ea688ee0d0978d98e39ba782
Moch-Ngafifuddin <moch.ngafifuddin20@gmail.com>
2023-10-16 11:29:24 +0700

    Menambahkan file contoh1.php pada folder controller

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Moch.Ngafifuddin
+        Saya tinggal di banyumas dan saya
+        tertarik dalam bidang programing";
+    }
+}
